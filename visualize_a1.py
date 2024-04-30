93b54b39fd911b9f5b63419aa0e5c6a4bee174fb
DameDollaForThree <j1cai@ucsd.edu>
2024-04-30 13:02:54 -0700

    small change

--- visualize_a1.py
+++ visualize_a1.py
@@ -30,7 +30,7 @@ def show_rollout_traj(rollout_traj, tag):
         env_name="a1_mimic",
         reference_traj=rollout_traj,
     )
-    components.html(html.render(env.sys.replace(dt=0.002*20), rollout_qp), height=500)
+    components.html(html.render(env.sys.replace(dt=env.dt), rollout_qp), height=500)
 
 
 def main():
